a615aa48ae4604a561f92ad10e41580a6d45fb68
Yanug1516 <55370519+Yanug1516@users.noreply.github.com>
2025-08-13 10:45:39 +0800

    index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wx6bd350b7cf90bcdc",
+  appsecret: "25b93fc5b14018d5f761528ed6d6df28",
+  template_id: "20RlsLZx0iP6RGRD1Ovn6TgLMVkOiptfsPc7HbBQxK4",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "杭州",
+  first_date: "2024-02-24",
 };
